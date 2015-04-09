4c6ff360c66cf2aaa7b3f70781d373af511e9223
Hackbright Student <info@hackbrightacademy.com>
2015-04-09 16:34:03 -0700

    completed first_three, last_five, and middle functions

--- list_operations.py
+++ list_operations.py
@@ -66,7 +66,7 @@ def first_three(input_list):
     [ A, B, C, D, E, F ] --> [ A, B, C ]
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -75,7 +75,7 @@ def last_five(input_list):
     [ A, B, C, D, E, F ] --> [ B, C, D, E, F ]
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -84,7 +84,7 @@ def middle(input_list):
     [ A, B, C, D, E, F ] --> [ C, D ]
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
