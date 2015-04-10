3ab6cdcbf70f6e42d78d097663ff422bc976fdf9
Hackbright Student <info@hackbrightacademy.com>
2015-04-09 17:19:07 -0700

    completed replace_head and replace_third_and_last

--- list_operations.py
+++ list_operations.py
@@ -111,8 +111,8 @@ def replace_head(input_list):
     Replace the head of the input list with the value 42.
     [ A, B, C, D ] --> [ 42, B, C, D]
     """
-
-    pass
+    input_list[0] = 42
+    return input_list
 
 
 def replace_third_and_last(input_list):
@@ -120,8 +120,8 @@ def replace_third_and_last(input_list):
     Replace the third and last elements of the input list with the value 37.
     [ A, B, C, D, E, F ] --> [ A, B, 37, D, E, 37 ]
     """
-
-    pass
+    input_list[2] = input_list[-1] = 37
+    return input_list
 
 
 def replace_middle(input_list):
